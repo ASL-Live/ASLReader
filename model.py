6168a134e33e0526b4cb7a4db2165a0ce8f675da
Leonardo Pariano <126412174+LeoPepperoni@users.noreply.github.com>
2024-03-20 13:56:06 -0400

    Update model.py

--- model.py
+++ model.py
@@ -23,6 +23,15 @@ def draw_landmarks(image, results):
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
 
 
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
+    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(1404)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+
+    return np.concatenate([pose, face, lh, rh])
+
+
 mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
 
@@ -50,4 +59,3 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
 
     cap.release()
     cv2.destroyAllWindows()
-
