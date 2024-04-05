288fa79432dd0a38850eca7b6508ad0762c494ce
famousfields <willfields92@gmail.com>
2024-04-05 12:44:31 -0400

    Added styled landmarks and improved model accuracy

--- dataCollection.py
+++ dataCollection.py
@@ -21,6 +21,30 @@ def draw_landmarks(image, results):
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
 
+def draw_styled_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+                              mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+                              mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
+                              )
+
+    # 2. Right hand
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+                              )
+
+    # 3. Left Hand
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+                              )
+
+    # 4. Pose Detections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                              )
+
 
 def extract_keypoints(results):
     pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
@@ -62,7 +86,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
                 print(results)
 
                 # Draw Landmarks
-                draw_landmarks(image, results)
+                draw_styled_landmarks(image, results)
 
                 if frame_num == 0:
                     cv2.putText(image, 'STARTING COLLECTION', (120, 200), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2,
--- model.py
+++ model.py
@@ -7,19 +7,20 @@ import mediapipe as mp
 from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.layers import LSTM, Dense, Dropout, Bidirectional
 from tensorflow.keras.callbacks import TensorBoard
+from keras.optimizers import Adam
+from keras.regularizers import l2
 from sklearn.metrics import multilabel_confusion_matrix, accuracy_score
 
 
 def mediapipe_detection(image, model):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # color conversion
     image.flags.writeable = False
-    results = model.process(image)
+    results = model.process(image)  # Make Prediction
     image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # color conversion
     return image, results
-
 
 def draw_landmarks(image, results):
     mp_drawing.draw_landmarks(image, results.face_landmarks)
@@ -27,41 +28,71 @@ def draw_landmarks(image, results):
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
 
-
 def extract_keypoints(results):
-    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
-    face = np.array([[res.x, res.y, res.z] for res in results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(1404)
-    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
-    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in
+                     results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
+    face = np.array([[res.x, res.y, res.z] for res in
+                     results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(1404)
+    lh = np.array([[res.x, res.y, res.z] for res in
+                   results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21 * 3)
+    rh = np.array([[res.x, res.y, res.z] for res in
+                   results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(
+        21 * 3)
     return np.concatenate([pose, face, lh, rh])
 
 
+def draw_styled_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+                              mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
+                              mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
+                              )
+
+    # 2. Right hand
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
+                              )
+
+    # 3. Left Hand
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
+                              )
+
+    # 4. Pose Detections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                              mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
+                              mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
+                              )
+
+
+# path for the exported data (numpy arrays)
 DATA_PATH = os.path.join('MP_Data')
+
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
 
 # Actions we want to detect
 actions = np.array(['hello', 'leo', 'my', 'name'])
-label_map = {label: num for num, label in enumerate(actions)}
 
+# thirty videos worth of data
 no_sequences = 30
+
+# videos are thirty frames in length
 sequence_length = 30
-sequences, labels = [], []
-
-log_dir = os.path.join('Logs')
-tb_callback = TensorBoard(log_dir=log_dir)
-
-model = Sequential()
-model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(30, 1662)))
-model.add(LSTM(128, return_sequences=True, activation='relu', ))
-model.add(LSTM(64, return_sequences=False, activation='relu', ))
-model.add(Dense(64, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(actions.shape[0], activation='softmax'))
 
 for action in actions:
     for sequence in range(no_sequences):
-        dir_path = os.path.join(DATA_PATH, action, str(sequence))
-        os.makedirs(dir_path, exist_ok=True)  # This will create all required parent directories
+        try:
+            os.makedirs(
+                os.path.join(DATA_PATH, action, str(sequence)))  # This will create all required parent directories
+        except:
+            pass
 
+label_map = {label:num for num, label in enumerate(actions)}
+
+#2 blank arrays( sequence = x data, labels = y data)
+sequences, labels = [], []
 for action in actions:
     for sequence in range(no_sequences):
         window = []
@@ -71,27 +102,63 @@ for action in actions:
         sequences.append(window)
         labels.append(label_map[action])
 
+#Set up data for training
 x = np.array(sequences)
 y = to_categorical(labels).astype(int)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.05)
 
-model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
-model.load_weights('model.h5')
-#model.fit(x_train, y_train, epochs=500, callbacks=[tb_callback])
+log_dir = os.path.join('Logs')
+tb_callback = TensorBoard(log_dir=log_dir)
+
+#model creation
+model = Sequential()
+model.add(Bidirectional(LSTM(128, return_sequences=True, activation='relu', input_shape=(30, 1662))))
+model.add(Dropout(0.2))
+model.add(LSTM(64, return_sequences=False, activation='relu', ))
+model.add(Dense(64, activation='relu', kernel_regularizer=l2(0.01)))
+model.add(Dense(32, activation='relu', kernel_regularizer=l2(0.01)))
+model.add(Dense(actions.shape[0], activation='softmax'))
+
+#set learning rate to train model slower
+learning_rate = .0001
+optimizer = Adam(learning_rate=learning_rate)
+model.compile(optimizer = optimizer, loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+
+#save weights
+#model.load_weights('model.h5') # if model was deleted and we want it back run this
+model.fit(x_train, y_train, epochs=170, callbacks=[tb_callback])
 #model.save('model.h5')
 
-mp_holistic = mp.solutions.holistic
-mp_drawing = mp.solutions.drawing_utils
+# Initialize `res` to prevent reference before assignment.
+# res = np.zeros(len(actions))
 
+yhat = model.predict(x_test)
+ytrue = np.argmax(y_test,axis=1).tolist()
+yhat = np.argmax(yhat,axis=1).tolist()
+multilabel_confusion_matrix(ytrue, yhat)
+
+#Display probability and visualize it while capturing video
+colors = [(245, 117, 16), (117, 245, 16), (16, 117, 245)]
+def prob_viz(res, actions, input_frame, colors):
+    output_frame = input_frame.copy()
+    for num, prob in enumerate(res):
+        cv2.rectangle(output_frame, (0, 60 + num * 40), (int(prob * 100), 90 + num * 40), colors[num], -1)
+        cv2.putText(output_frame, actions[num], (0, 85 + num * 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
+                    cv2.LINE_AA)
+
+    return output_frame
+
+
+#Detection variables
 sequence = []
 sentence = []
 threshold = 0.8
+
 
 cap = cv2.VideoCapture(0)
 
 # Initialize `res` to prevent reference before assignment.
 res = np.zeros(len(actions))
-
 # Set mediapipe model
 with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     while cap.isOpened():
@@ -101,7 +168,7 @@ with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=
             break
 
         image, results = mediapipe_detection(frame, holistic)
-        draw_landmarks(image, results)
+        draw_styled_landmarks(image, results)
 
         keypoints = extract_keypoints(results)
         sequence.append(keypoints)
