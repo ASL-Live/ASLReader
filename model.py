162783b51f07f7390ca821b7e9fa5c155f477da4
Leonardo Pariano <126412174+LeoPepperoni@users.noreply.github.com>
2024-08-26 14:18:16 -0400

    Split Training and Testing Functionality into Separate Scripts
    Training Script: Created a dedicated script for model training, which includes:
    
    Loading and preprocessing the data.
    Setting up the neural network model with LSTM layers, dropout, and dense layers.
    Training the model on the training dataset and saving the trained model for later use.
    Evaluating the model's performance on a separate test dataset and outputting key metrics such as the confusion matrix and accuracy score.
    Testing Script: A separate script was designed to:
    
    Load the trained model.
    Capture real-time video input.
    Process the video frames using MediaPipe to extract relevant landmarks.
    Use the trained model to predict actions based on the extracted keypoints, with results displayed both in the terminal and overlaid on the video feed.
    This change enhances code organization, making it easier to manage and scale the project by separating the concerns of training and real-time inference.

--- model.py
+++ model.py
@@ -1,9 +1,6 @@
-
 import cv2
 import numpy as np
 import os
-from matplotlib import pyplot as plt
-import time
 import mediapipe as mp
 from tensorflow.keras.utils import to_categorical
 from sklearn.model_selection import train_test_split
@@ -14,96 +11,59 @@ from keras.optimizers import Adam
 from keras.regularizers import l2
 from sklearn.metrics import multilabel_confusion_matrix, accuracy_score
 
-
 def mediapipe_detection(image, model):
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # color conversion
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     image.flags.writeable = False
-    results = model.process(image)  # Make Prediction
+    results = model.process(image)
     image.flags.writeable = True
-    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)  # color conversion
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
     return image, results
 
 def draw_landmarks(image, results):
-    mp_drawing.draw_landmarks(image, results.face_landmarks)
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)
+    mp_drawing.draw_landmarks(image, results.pose_landmarks)
     mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
     mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)
 
 def extract_keypoints(results):
-    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in
-                     results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
-    face = np.array([[res.x, res.y, res.z] for res in
-                     results.face_landmarks.landmark]).flatten() if results.face_landmarks else np.zeros(1404)
-    lh = np.array([[res.x, res.y, res.z] for res in
-                   results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21 * 3)
-    rh = np.array([[res.x, res.y, res.z] for res in
-                   results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(
-        21 * 3)
-    return np.concatenate([pose, face, lh, rh])
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(132)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([pose, lh, rh])
 
-
-def draw_styled_landmarks(image, results):
-    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
-                              mp_drawing.DrawingSpec(color=(80, 110, 10), thickness=1, circle_radius=1),
-                              mp_drawing.DrawingSpec(color=(80, 256, 121), thickness=1, circle_radius=1)
-                              )
-
-    # 2. Right hand
-    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                              mp_drawing.DrawingSpec(color=(80, 22, 10), thickness=2, circle_radius=4),
-                              mp_drawing.DrawingSpec(color=(80, 44, 121), thickness=2, circle_radius=2)
-                              )
-
-    # 3. Left Hand
-    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
-                              mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4),
-                              mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2)
-                              )
-
-    # 4. Pose Detections
-    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
-                              mp_drawing.DrawingSpec(color=(245, 117, 66), thickness=2, circle_radius=4),
-                              mp_drawing.DrawingSpec(color=(245, 66, 230), thickness=2, circle_radius=2)
-                              )
-
-
-# path for the exported data (numpy arrays)
+# Path for the exported data (numpy arrays)
 DATA_PATH = os.path.join('MP_Data')
 
 mp_holistic = mp.solutions.holistic
 mp_drawing = mp.solutions.drawing_utils
 
 # Actions we want to detect
-actions = np.array(['hello', 'leo', 'my', 'name'])
+actions = np.array(['hello', 'thank you'])
 
-# thirty videos worth of data
-no_sequences = 30
+label_map = {label: num for num, label in enumerate(actions)}
 
-# videos are thirty frames in length
-sequence_length = 30
-
-for action in actions:
-    for sequence in range(no_sequences):
-        try:
-            os.makedirs(
-                os.path.join(DATA_PATH, action, str(sequence)))  # This will create all required parent directories
-        except:
-            pass
-
-label_map = {label:num for num, label in enumerate(actions)}
-
-#2 blank arrays( sequence = x data, labels = y data)
+# Arrays to hold sequences and labels
 sequences, labels = [], []
+
+# Iterate over each action
 for action in actions:
-    for sequence in range(no_sequences):
+    action_path = os.path.join(DATA_PATH, action)
+    sequences_folders = os.listdir(action_path)
+
+    # Iterate over each sequence folder in the action directory
+    for sequence_folder in sequences_folders:
         window = []
-        for frame_num in range(sequence_length):
-            res = np.load(os.path.join(DATA_PATH, action, str(sequence), "{}.npy".format(frame_num)))
+        sequence_path = os.path.join(action_path, sequence_folder)
+
+        # Iterate over all frame files in the sequence folder
+        for frame_file in sorted(os.listdir(sequence_path), key=lambda x: int(x.split('.')[0])):
+            frame_path = os.path.join(sequence_path, frame_file)
+            res = np.load(frame_path)
             window.append(res)
+
         sequences.append(window)
         labels.append(label_map[action])
 
-#Set up data for training
+# Set up data for training
 x = np.array(sequences)
 y = to_categorical(labels).astype(int)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.05)
@@ -111,86 +71,31 @@ x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.05)
 log_dir = os.path.join('Logs')
 tb_callback = TensorBoard(log_dir=log_dir)
 
-#model creation
+# Model creation
 model = Sequential()
-model.add(Bidirectional(LSTM(128, return_sequences=True, activation='relu', input_shape=(30, 1662))))
+model.add(Bidirectional(LSTM(128, return_sequences=True, activation='relu', input_shape=(x_train.shape[1], x_train.shape[2]))))
 model.add(Dropout(0.2))
-model.add(LSTM(64, return_sequences=False, activation='relu', ))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
 model.add(Dense(64, activation='relu', kernel_regularizer=l2(0.01)))
 model.add(Dense(32, activation='relu', kernel_regularizer=l2(0.01)))
 model.add(Dense(actions.shape[0], activation='softmax'))
 
-#set learning rate to train model slower
-learning_rate = .0001
+# Set learning rate to train the model slower
+learning_rate = 0.0001
 optimizer = Adam(learning_rate=learning_rate)
-model.compile(optimizer = optimizer, loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+model.compile(optimizer=optimizer, loss='categorical_crossentropy', metrics=['categorical_accuracy'])
 
-#save weights
-#model.load_weights('model.h5') # if model was deleted and we want it back run this
+# Train the model
 model.fit(x_train, y_train, epochs=170, callbacks=[tb_callback])
-#model.save('model.h5')
 
-# Initialize `res` to prevent reference before assignment.
-# res = np.zeros(len(actions))
-
+# Evaluate the model
 yhat = model.predict(x_test)
-ytrue = np.argmax(y_test,axis=1).tolist()
-yhat = np.argmax(yhat,axis=1).tolist()
-multilabel_confusion_matrix(ytrue, yhat)
+ytrue = np.argmax(y_test, axis=1).tolist()
+yhat = np.argmax(yhat, axis=1).tolist()
 
-#Display probability and visualize it while capturing video
-colors = [(245, 117, 16), (117, 245, 16), (16, 117, 245)]
-def prob_viz(res, actions, input_frame, colors):
-    output_frame = input_frame.copy()
-    for num, prob in enumerate(res):
-        cv2.rectangle(output_frame, (0, 60 + num * 40), (int(prob * 100), 90 + num * 40), colors[num], -1)
-        cv2.putText(output_frame, actions[num], (0, 85 + num * 40), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2,
-                    cv2.LINE_AA)
+# Display evaluation metrics
+print(multilabel_confusion_matrix(ytrue, yhat))
+print(f'Accuracy: {accuracy_score(ytrue, yhat)}')
 
-    return output_frame
-
-
-#Detection variables
-sequence = []
-sentence = []
-threshold = 0.8
-
-
-cap = cv2.VideoCapture(0)
-
-# Initialize `res` to prevent reference before assignment.
-res = np.zeros(len(actions))
-# Set mediapipe model
-with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if not ret:
-            print("Failed to grab frame")
-            break
-
-        image, results = mediapipe_detection(frame, holistic)
-        draw_styled_landmarks(image, results)
-
-        keypoints = extract_keypoints(results)
-        sequence.append(keypoints)
-        sequence = sequence[-30:]
-
-        if len(sequence) == 30:
-            res = model.predict(np.expand_dims(sequence, axis=0))[0]
-
-        if res[np.argmax(res)] > threshold:
-            action = actions[np.argmax(res)]
-            if len(sentence) == 0 or (sentence[-1] != action):
-                sentence.append(action)
-
-        sentence = sentence[-5:]  # Keep the last 5 actions
-
-        cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
-        cv2.putText(image, ' '.join(sentence), (3, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
-
-        cv2.imshow('Feed', image)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-cap.release()
-cv2.destroyAllWindows()
+# Save the trained model
+model.save('model.h5')
